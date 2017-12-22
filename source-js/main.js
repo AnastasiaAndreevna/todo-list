44bd09b3946124a67379790c2a4f071658419f95
AnastasiaAndreevna <31144016+AnastasiaAndreevna@users.noreply.github.com>
2017-12-22 20:21:12 +0200

    amended commit

--- source-js/main.js
+++ source-js/main.js
@@ -1,34 +1,32 @@
-jQuery(function () {
-  var jQuerytasksList = jQuery('#tasksList')
-  var jQuerytasksInput = jQuery('#tasksInput')
-  var jQuerynotification = jQuery('#notification')
+var jQuerytasksList = jQuery('#tasksList')
+var jQuerytasksInput = jQuery('#taskInput')
+var jQuerynotification = jQuery('#notification')
 
-  var displayNotification = function () {
-    if (!jQuerytasksList.children().length) {
-      jQuerynotification.fadeIn('fast')
-    } else {
-      jQuerynotification.css('display', 'none')
-    }
+var displayNotification = function () {
+  if (!jQuerytasksList.children().length) {
+    jQuerynotification.fadeIn('fast')
+  } else {
+    jQuerynotification.css('display', 'none')
   }
+}
 
-  jQuery('#taskAdd').on('click', function () {
-    if (!jQuerytasksInput.val()) {return false}
+jQuery('#taskAdd').on('click', function () {
+  if (!jQuerytaskInput.val()) {return false}
 
-    jQuerytasksList.append('<li>' + jQuerytasksInput.val() + '<button class=\'delete\'>&#10006</button></li>')
+  jQuerytasksList.append('<li>' + jQuerytaskInput.val() + '<button class=\'delete\'>&#10006</button></li>')
 
-    jQuerytasksInput.val('')
+  jQuerytaskInput.val('')
 
-    displayNotification()
+  displayNotification()
 
-    jQuery('.delete').on('click', function () {
-      var jQueryparent = jQuery(this).parent()
+  jQuery('.delete').on('click', function () {
+    var jQueryparent = jQuery(this).parent()
 
-      jQueryparentparent.css('animation', 'fadeOut.3s linear')
+    jQueryparentparent.css('animation', 'fadeOut.3s linear')
 
-      setTimeout(function () {
-        jQueryparent.remove()
-        displayNotification()
-      }, 295)
-    })
+    setTimeout(function () {
+      jQueryparent.remove()
+      displayNotification()
+    }, 295)
   })
 })
